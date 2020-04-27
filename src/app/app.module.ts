c4a292715c8b21768ede74e1ce5ccdd672069d9c
Evgeni Konstantinov <evgeni.k@burns.app>
2020-04-27 20:57:18 +0300

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule.forRoot(), SharedModule.forRoot()],
+  imports: [BrowserModule, AppRoutingModule, CoreModule.forRoot(), SharedModule.forRoot(), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
